16a75c96261a619a8ecb7c527274a8696dc3f408
RuhamaMalik <malikruhama7@gmail.com>
2026-01-05 13:01:06 +0500

    js alert , dev tools, console.log, start variables

---
+++ js/class-01/index.js
@@ -0,0 +1,11 @@
+// alert("External js file!");
+
+// console.log("External js file!");
+
+/////////////// Variables
+
+
+var  x;   // Declare 
+x = "hello"; // Initialization / assign ---- = --> Assignment operator
+
+x= "ruhama" /// reassign
