93a57cc3a19d7b3fedbf29689e32cd4436ca693b
RuhamaMalik <malikruhama7@gmail.com>
2026-01-07 13:00:34 +0500

    math expression familiar operator start

---
+++ js/class-02/index.js
@@ -0,0 +1,149 @@
+/////////////// Variables (var, let, const)
+
+// var username;   // declare
+// username = "Ruhama"; // initialization
+
+// var username = "Ruhama";
+
+// console.log(username);
+
+// username = "Gull"; // reassign
+
+// console.log(username);
+// console.log(username);
+// console.log(username);
+// console.log(username);
+// console.log(username);
+// console.log(username);
+
+// username = "HA Collection";
+// console.log(username);
+
+
+
+// var x ;
+// console.log(x); // undefined
+
+
+
+/////////////////////////////// Data Types
+
+/*
+
+String ---------- '' , "" , `` ---- text
+Number ---------- 34 , 5.5 
+Boolean --------- true / false
+undefined ------- 
+null ------- 
+
+var x = 5;
+
+//////// Reference DataTypes
+
+Object ---------- {key:value, name:"ruhama"}
+Array ----------- [5 , 6,"ruhama"]
+
+
+*/
+
+// var message = "Hello Bacho!";
+// alert(message);
+
+
+// var age = 10;
+// var price = 100;
+
+// console.log(price);
+
+// price = price + 10;
+
+// console.log(price);
+
+// var newPrice = price + 20;
+
+// console.log(price);
+// console.log(newPrice);
+
+////////////////////
+
+
+// var x = "15";
+// var y = 5;
+// var total = x + y;
+
+
+// console.log(total); // 155
+
+/////////////////
+
+// var isLoggedIn = false;
+// var isActive = false;
+
+///////////////// JS Naming Convention
+
+// camelCase
+// snake_case
+// PascalCase
+
+////////////////
+
+// x =10;
+// console.log(x);
+
+////////////////
+
+// console.log(x);  // undefined
+// var x = 5;
+
+
+// console.log(x); // error
+// const x = 5;
+
+// var price = 10;
+// price = 15;
+
+// const pi = "3.142";
+// pi="384837483"
+// console.log(pi); // error
+
+///////////////// 4-Variable Names Legal and Illegal
+
+////////// legal (aA-zZ, $ , _ ,  )
+
+var UserNamE;
+
+var $myVar;
+var myVar$;
+var my$var;
+
+var _myVar;
+var my_var;
+var myVar_;
+
+var my1stVar;
+var myVar1;
+var myvar;
+var myAlert;
+
+////////// illegal (special character, 1user, reserve keyword)
+
+// var 1myVar$;  
+// var my var;
+// var my-var
+// var myVar#1
+// var var;
+
+
+// var x =5;
+// console.log(X); // case sensitive
+
+
+
+/////////// Math expressions: - Familiar operators (+, - , * , / , %)
+
+var num = 5;
+
+console.log(num + 3); // 8
+console.log(5 - 3); // 2
+console.log(5 * 3); // 15
+console.log(15 / num); // 3
