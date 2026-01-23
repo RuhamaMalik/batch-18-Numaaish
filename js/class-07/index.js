76bc549494f0e9a972f761657c07ec910ca4015e
RuhamaMalik <malikruhama7@gmail.com>
2026-01-23 12:45:27 +0500

    prompt , comparision operator

---
+++ js/class-07/index.js
@@ -0,0 +1,73 @@
+//////////////////// Prompts 
+
+// prompt(question, defaultAns(optional))
+
+
+// var colour = prompt("What is your Favourite colour?");
+
+// alert("Your Favourite colour is " +  colour)
+
+
+
+// var favNum = +prompt("PlEASE enter your Favourite Number");
+
+// console.log(favNum + 5); // 55
+// console.log(favNum + 5); // 10
+// NaN -- Not a Number
+
+
+
+// var country = prompt("Enter your country", "Pakistan");
+
+// console.log(country);
+
+// prompt("223190 + 1") 
+
+////////////////// Comparision Operaotors 
+/*
+compare 2 values 
+result Boolean
+
+== --- checkValues !dataType
+!=
+
+=== 
+!==
+
+> ---- greater than
+< ---- less than
+>= --- greater than or equals to
+<= --- less than or equals to
+
+*/
+// console.log(5 == 5); // true
+// console.log(6 != 5); // true
+// console.log(5 == "5"); // true
+// console.log(4 != "5"); // true
+
+
+
+// console.log(5 === 5); // true
+// console.log("5" === 5); // false
+// console.log(5 !== 5); // false
+// console.log(6 !== 5); // true
+// console.log("5" !== 5); // true
+
+// console.log(5 > 6); // false
+// console.log(6 > 6); // false
+// console.log("5" > 4); // true
+
+
+// console.log("5" < 4); // false
+// console.log(6 < 7); // true
+
+
+// console.log(5 >= 5); // true
+// console.log(5 >= 4); // true
+// console.log(7 >= 4); // true
+// console.log("7" >= 4); // true
+// console.log(3 >= 4); // false
+
+
+
+
