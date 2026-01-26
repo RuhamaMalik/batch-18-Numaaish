6be5fffadfd37dcea1d3e7b366fd1e38d540464b
RuhamaMalik <malikruhama7@gmail.com>
2026-01-26 12:59:53 +0500

    if , else

---
+++ js/class-08/index.js
@@ -0,0 +1,150 @@
+//////////////////// 10 - if statements
+
+// console.log("Aloo k parathy");
+
+
+/*
+
+if(condition){
+// statement 
+}
+
+*/
+
+
+//////////// truthy or falsy values
+
+// if(1){
+//      console.log("Mai agaya!");
+// }
+
+// if(55){
+//   console.log("Mai agaya!");
+// }
+
+// if(-1){
+//   console.log("Mai agaya!");
+// }
+
+// if(" "){
+//    console.log("Mai agaya!");
+// }
+
+
+// if("ruhama"){
+//    console.log("Mai agaya!");
+// }
+
+
+// if("false"){
+//  console.log("Mai agaya!");
+// }
+
+// if("undefined"){
+//   console.log("Mai agaya!");
+// }
+
+////////////////// false, 0, "",null, undefined
+
+
+
+// if(false){
+//    console.log("Aloo k parathy");
+// }
+
+// if(0){
+//      console.log("Mai agaya!");
+// }
+// if(""){
+//    console.log("Mai agaya!");
+// }
+
+// if (null) {
+//   console.log("Mai agaya!");
+// }
+
+// if(undefined){
+//   console.log("Mai agaya!");
+// }
+
+
+/////////////////////////// if
+
+// const isRain = true;
+// if (isRain) {
+//   console.log("Aloo k parathy");
+// }
+
+
+// if (5 > 4) {
+//   console.log("Pagal");
+// }
+
+////////////////
+
+// let city = prompt("What is the capital of Pakistan?");
+// let correctAns = "islamabad";
+
+// if (city === correctAns) {
+//   alert("Congratulations! Correct Answer! " + city)
+// } else {
+//   alert("Wrong Answer! Correct Ans is : " + correctAns)
+// }
+
+
+////////////////
+
+// const isRain = true;
+
+// if (isRain) {
+//   console.log("Aloo k parathy!");
+// } else {
+//   console.log("Chicken Biryani");
+// }
+
+////////////////
+
+// let number = +prompt("Enter Number");
+// if (number % 2 === 0) {
+//   console.log(number + " is Even Number!");
+// } else {
+//   console.log(number + " is Odd Number!");
+// }
+
+
+/////////////////////// else if
+
+// var colour = prompt("Enter colour name");
+
+// if (colour === "red") {
+//   console.log("Colour is Red");
+// }
+// else if (colour === "green") {
+//   console.log("Colour is green");
+// }
+// else if (colour === "yellow") {
+//   console.log("Colour is yellow");
+// } else {
+//   console.log("Another Colour!");
+// }
+
+
+/////////////////////////////
+
+var number = +prompt("Enter Number");
+
+if (number > 10) {
+  console.log("Greater than 10!");
+} else if (number < 10) {
+  console.log("Less than 10");
+} else if (number == 10) {
+  console.log("Equals to 10");
+} else {
+  console.log("Invalid Input");
+}
+
+
+
+
+
+
