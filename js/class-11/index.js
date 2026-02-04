2febdb2d8ea83f64bee1e7d7f90723bb332606b1
RuhamaMalik <malikruhama7@gmail.com>
2026-02-04 13:02:10 +0500

    array starts

---
+++ js/class-11/index.js
@@ -0,0 +1,34 @@
+////////////////////// Arrays
+
+
+// var user1 = "Ruhama"
+// var user2 = "Ayesha"
+// var user3 = "Hajra"
+// var user4 = "Fatima"
+// var user5 = "Fakiha"
+
+
+// var myArr = new Array("Apple","Orange");
+// console.log(myArr);
+
+// var students = ["Asma", "Kahfilwara", "Noor"];
+// console.log(students);
+
+// var myArr = [
+//   "myString",
+//   123,
+//   undefined,
+//   null,
+//   false,
+//   {},
+//   [1,2,3]
+// ]
+
+var arr = ["mango","orange","strawberry"];
+console.log(arr);
+console.log(arr.length); // 3
+// length : No Of eLements --- 3 , start:1
+// index : position of an element , start 0
+
+
+
---
+++ js/class-12/index.js
@@ -0,0 +1,63 @@
+// 80 -- A+
+// 70 -- A
+// 60 -- B
+// 50 -- C
+// Fail
+
+
+// 101 - -1  ---- Invalid Percentage
+
+// let perc = +prompt("Enter your percentage");
+
+// if ((perc <= 100) && (perc >= 0)) {
+//   if (perc >= 80) {
+//     console.log("A+");
+//   } else if (perc >= 70) {
+//     console.log("A");
+//   } else if (perc >= 60) {
+//     console.log("B");
+//   } else if (perc >= 50) {
+//     console.log("C");
+//   } else {
+//     console.log("Fail");
+//   }
+// } else {
+//   console.log("Invalid Percentage!");
+// }
+
+
+////////////////////
+
+let teachers = [
+  "miss ruhama",
+  "miss bisma",
+  "miss nooruleman",
+  "miss hafsa"
+];
+
+// length : 4
+// index : 0 - 3
+
+// console.log(teachers);
+// console.log(teachers.length); // 4
+
+// console.log(teachers[1]);
+// console.log(teachers[2]);
+// console.log(teachers[5]); // undefined
+
+// teachers[0] = "miss ruhama gull";
+
+
+
+// teachers[3] = "miss hafsa nadeem";
+// teachers[4] = "miss hira";
+
+// teachers[6] = "miss tayyaba";
+// console.log(teachers.length);
+
+// teachers.length = 0;
+// teachers = [];
+// teachers[3] = "miss tuba";
+// console.log(teachers.length); // 4
+
+
