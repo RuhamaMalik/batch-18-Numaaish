125d322f3c1383eecda0e8764afbdf28d58f1055
RuhamaMalik <malikruhama7@gmail.com>
2026-02-16 19:06:11 +0500

    loops

---
+++ js/class-16-17/index.js
@@ -0,0 +1,111 @@
+// let cities = ["karachi", "peshawar", "islamabad", "quetta", "lahore"];
+// let cleanestCity = prompt("Enter cleanest city!");
+
+// let isCityFound = false;
+
+// for (let i = 0; i < cities.length; i++) {
+//   if (cleanestCity === cities[i]) {
+//     isCityFound = true;
+//     console.log("City Found!");
+//     break;
+//   }
+//   // else{
+//   //   console.log("city not found");
+//   // }
+// }
+
+// if(isCityFound === false){
+// console.log("city not found");
+// }
+
+
+///////////////////
+
+
+// let arr = [["a", "b", "c"], [1, 2, 3, 4, 5], ["x", "y"]];
+
+// //           3
+// for (var i = 0; i < arr.length; i++) {
+
+//   //         1
+//   for (var j = 0; j < arr[i].length; j++) {
+//     console.log(arr[i][j]); // arr[2][1]  y
+//   }
+
+// }
+
+
+/////////////////// print counting
+
+//           21
+// for (var i = 1; i <= 100; i += 10) {
+//   // document.writeln(`${i} <br>`);
+//   //           21     31
+//   for (var j = i; j < i + 10; j++) {
+//     document.writeln(`${j}`); 21 - 30
+
+//   }
+
+
+//   document.writeln("<br>")
+// }
+
+
+
+
+
+
+// for(var i=100; i>=1;i-=10){
+//   for(var j =i;j>i-10;j--){
+//     document.writeln(j);
+//   }
+//   document.writeln("<br>")
+// }
+
+
+/////////////////////// print fullname
+
+// var fName = ["ayesha", "hania", "maheen", "maham", "hina"];
+// var lName = ["ghulam", "amir", "imran", "wahab", "altaf"];
+// var fullName = [];
+
+
+// for (let i = 0; i < fName.length; i++) {
+//  fullName.push(fName[i] + " "+ lName[i]);
+// }
+
+// console.log(fullName);
+
+
+/////////////  print skills with name
+
+
+// var employes = ["ayesha", "hania", "maheen", "maham", "hina"];
+// var skills = ["javaScript", "html", "css", "bootstrap", "github"];
+
+// for (var i = 0; i < employes.length; i++) {
+//   document.writeln(`<h1>${employes[i]}</h1>`);
+
+//   for (var j=0; j<skills.length; j++) {
+//   document.writeln(`<li>${skills[j]}</li>`);
+
+//   }
+
+
+// }
+
+
+// let odd = [];
+
+// for (var i = 1; i <= 100; i++) {
+//   if (i % 2 === 0) {
+//     odd.push(i);
+//   }
+// }
+
+// console.log(odd);
+
+
+
+
+
