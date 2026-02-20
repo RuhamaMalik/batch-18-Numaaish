424a6dc47a3eac0afc08c72c894bfce78548ce91
RuhamaMalik <malikruhama7@gmail.com>
2026-02-20 11:48:11 +0500

    string methods

---
+++ js/class-19/index.js
@@ -0,0 +1,32 @@
+// let str = `JavaScript (JS) is a versatile, high-level programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS. It enables developers to create interactive and dynamic web experiences. 
+// Wikipedia
+// Wikipedia
+//  +2
+// Key Concepts
+// Function: JavaScript makes static HTML and CSS pages dynamic by responding to user interactions, updating content, and validating data without requiring a full page reload.
+// Execution Environments: Originally a client-side scripting language running in the browser, modern JavaScript (via runtimes like Node.js) can now also be used for server-side development, game development, and mobile applications (with frameworks like React Native).`;
+
+// let text = prompt("Enter a text").toLowerCase(); // abcd
+// let newText = prompt("Enter a new text"); // xyz
+// let isTextFound = false;
+
+// for (var i = 0; i < str.length; i++) {
+//   if (str.slice(i, i + text.length).toLowerCase() === text) {
+//     isTextFound=true;
+//     str = str.slice(0, i) + newText + str.slice(i + text.length)
+//   }
+// }
+
+
+// if(!isTextFound){
+// console.log("Text Not Found");
+// }
+
+// console.log(str);
+
+let name = "Ruhama"
+// console.log("Ruhama".charAt(0)); // R
+console.log(name.charAt(name.length-1)); // a
+
+
+
