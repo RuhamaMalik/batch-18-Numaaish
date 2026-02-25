6caa61b8903b5213cc8a625a8a439c85346233cb
RuhamaMalik <malikruhama7@gmail.com>
2026-02-25 12:04:25 +0500

    convert str into int

---
+++ js/class-21/index.js
@@ -0,0 +1,16 @@
+// let str = "5.46";
+// console.log(typeof str, str+5);
+
+// let num = parseInt(str);
+// console.log(typeof  num, num+5);
+
+// console.log(parseInt("34.99")); // 34
+// console.log(parseInt("13 45") ); // 13 number
+// console.log(parseInt("13 45" + 1) ); // 13 number
+// console.log(parseInt("10 years")); // 10
+
+// console.log(parseInt("1" + 1)); // 11
+
+/////// radix --- number system
+// console.log(parseInt("1" + 1, 2)); // 11
+
