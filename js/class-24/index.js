1cb6fb7b5340505801c1b3e7189e7ae2357f56b6
RuhamaMalik <malikruhama7@gmail.com>
2026-03-06 11:46:49 +0500

    function complete

---
+++ js/class-24/index.js
@@ -0,0 +1,67 @@
+// let x = "outiside function";
+
+// random();
+// function random() {
+//   let x = "inside function"; // variable shadowing
+//   console.log(x); 
+// }
+
+
+// console.log(x); // 
+
+
+////////////////////////// 
+
+// let x = "outiside function";
+
+
+// function random() {
+//   x = "inside function";  // reassign
+//   console.log(x); 
+// }
+
+
+// random(); // inside
+// console.log(x); // inside
+
+
+// console.log(x); // outside
+// random(); // inside
+// console.log(x); // inside
+
+
+///////////////////////// return
+
+// function totalAmount(a, b) {
+//   let total = a + b
+//   // return total;
+//   return (
+//    total
+//   )
+// }
+
+// // totalAmount(3, 6);
+
+// let deliveryPrice = 150;
+// let result = totalAmount(3, 6) + deliveryPrice ;
+
+// console.log(result);
+
+
+/////////////////////////
+
+// function kuchBhi (){
+//   return "Hello Bacho!";
+//   // let a = 5;
+//   // return "How aRE you?"
+// }
+
+
+// let str = kuchBhi();
+// console.log(str);
+
+
+/////////////////// 39 - switch statements
+
+
+
