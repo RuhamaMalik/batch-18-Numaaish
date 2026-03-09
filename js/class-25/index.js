69bd6f31e73a74b501aa4451e17458cd5971ded8
RuhamaMalik <malikruhama7@gmail.com>
2026-03-09 12:04:23 +0500

    start events

---
+++ js/class-25/index.js
@@ -0,0 +1,71 @@
+///////////// switch statements
+
+// let signals = prompt("enter signals colors");
+
+// switch(signals){
+//     case "red" : console.log("Stop");
+//     break;
+
+//     case "yellow" : console.log("Ready to go");
+//     break;
+
+//     case "green" : console.log("Go");
+//     break;
+
+//    default : console.log("Entr a valid signal color");
+// }
+
+// let age = +prompt("Enter your age");
+
+// switch(true){
+//   case (age >= 1 && age<= 12) : 
+//   console.log("child")
+//   break;
+
+//   case (age >= 13 && age<= 19) : 
+//   console.log("Teenage");
+//   break;
+
+//   case (age >= 20 && age<= 45) : 
+//   console.log("Young");
+//   break;
+
+//   default : console.log("bonus...");
+
+// }
+
+// let day = prompt("Enter a day!").toLowerCase();
+
+// switch (day) {
+//   case "sunday":
+//   case "saturday":
+//     console.log("Weekend!");
+//     break;
+//   case "monday":
+//   case "tuesday":
+//   case "wednesday":
+//   case "thursday":
+//   case "friday":
+//     console.log("Working Day!");
+//     break;
+//   default: console.log("Mai zinda hun!");
+// }
+
+/*
+Multi 
+line
+comment
+
+occurance
+
+*/
+// console.log("external");
+
+
+/////////////// Events
+
+
+function greet (){
+  alert(`Hello Ruhama`);
+}
+
