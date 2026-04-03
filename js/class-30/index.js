1987a4919e5238d25bd0762395a249f18001801f
RuhamaMalik <malikruhama7@gmail.com>
2026-04-03 12:46:20 +0500

    quiz + start object

---
+++ js/class-30/index.js
@@ -0,0 +1,25 @@
+//////////////// Object --- reference datatype, {}, key:value pair
+
+// let user = ['ruhama',10,10,'morning'];
+
+// let obj = {
+   // propertyName:'propertyValue',
+  // key:'value',
+// }
+
+let user = {
+  type:'fake data',
+  name:'Ruhama',
+  age:10,
+  hobbies:['reading','eating','sleeping'],
+  isActive:true,
+  address:{
+    stree:'street 1',
+    appartment:'2',
+    zipCode:'0001',
+    city:'karachi',
+    country:'pakistan'
+  }
+}
+
+console.log(user);
