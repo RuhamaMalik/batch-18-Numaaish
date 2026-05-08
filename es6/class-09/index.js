4f8c80e0571503e0944bc8aec8464fe594daa8ff
RuhamaMalik <malikruhama7@gmail.com>
2026-05-08 12:41:40 +0500

    generator complete

---
+++ es6/class-09/index.js
@@ -0,0 +1,43 @@
+// function* infinite() {
+//   let num = 1;
+//   while (true) {
+//     yield num++;
+//   }
+
+// }
+
+// let i = infinite();
+
+// for (let abcd of i) {
+//   if (abcd === 5 || abcd === 7) {
+//     continue;
+//   }
+//   else if (abcd <= 10) {
+//     console.log(abcd);
+//   } else {
+//     break;
+//   }
+// }
+
+
+/////////////////////
+
+// function* khatam(){
+//   // yield* {course:"MODERN WEB DEVELOPMENT"}; // error
+//   yield* 'hello bacho!';
+//   yield* ["apple", "banana","mango","orange","grapes"];
+//   yield "tata bye bye!"
+// }
+
+// const g = khatam();
+
+// console.log(g.next());
+// console.log(g.next());
+// console.log(g.next());
+
+
+/////////////////////// array methods
+
+//////////// 
+
+
