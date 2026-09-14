13220c093bb1fc83c4c4bb2afe4568baf2579873
RuhmaMalik <malikruhama7@gmail.com>
2026-09-14 13:05:20 +0500

    props

--- React/class-01-02/src/Img.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Img = () => {
-  return (
-    <h1>Img Component</h1>
-  )
-}
-
-export default Img
---
+++ React/class-01-03/src/components/Cart.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './Button'
+
+const Cart = () => {
+  return (
+  <>
+  <li>Shoes</li>
+  <li>Laptop  </li>
+  <Button myTitle='Checkout' />
+  </>
+  )
+}
+
+export default Cart
